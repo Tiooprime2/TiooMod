a8a0f3e784478ba9f5bea5668f8fb71c2a7b785d
Tiooprime2 <hrid9112@gmail.com>
2026-03-11 17:51:02 +0800

    Add files via upload

--- src/main/java/net/tioo/mod/modules/Module.java
+++ src/main/java/net/tioo/mod/modules/Module.java
@@ -1,1 +1,29 @@
+package net.tioo.mod.modules;
 
+public abstract class Module {
+
+    private final String name;
+    private final String description;
+    private boolean enabled = false;
+
+    public Module(String name, String description) {
+        this.name        = name;
+        this.description = description;
+    }
+
+    public void toggle() {
+        enabled = !enabled;
+        if (enabled) onEnable();
+        else         onDisable();
+    }
+
+    public void onEnable()  {}
+    public void onDisable() {}
+
+    public String  getName()        { return name; }
+    public String  getDescription() { return description; }
+    public boolean isEnabled()      { return enabled; }
+    public void    setEnabled(boolean v) {
+        if (v != enabled) toggle();
+    }
+}
